c5367d6f4dc2f3a1007ad7e70ac9bee8d5ba9253
Roshan Tamang <roshantamang@hotmail.co.uk>
2023-03-26 23:22:13 +0100

    Added unit tests for Tile

--- src/test/java/com/softwareintitute/rt/minesweeper/TileTest.java
+++ src/test/java/com/softwareintitute/rt/minesweeper/TileTest.java
@@ -1,20 +1,60 @@
 package com.softwareintitute.rt.minesweeper;
 
+import com.softwareintitute.rt.minesweeper.game.data.Tile;
 import org.junit.jupiter.api.Test;
 
-public class TileTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class TileTest {
 
 
     @Test
-    void testing_tile_bomb(){
+    void testing_tile_bomb() {
 
-        
+        Tile tile = new Tile();
+
+        tile.setABomb(true);
+
+        assertTrue(tile.isABomb(), "It should be true");
 
     }
 
     @Test
-    void testing_flag_tile(){
+    void testing_tile_flag() {
+
+        Tile tile = new Tile();
+
+        tile.flagTile();
+
+        assertTrue(tile.isFlagged(), "It should be true");
+
 
     }
+
+    @Test
+    void testing_tile_gettingNumberOfNeighbouringMines(){
+
+        Tile tile = new Tile();
+
+        tile.setNumberOfNeighbouringMines(2);
+
+        assertEquals(2,tile.getNumberOfNeighbouringMines(),"The values should match");
+
+    }
+
+
+    @Test
+    void testing_tile_revealTile(){
+
+        Tile tile = new Tile();
+
+        tile.revealTile();
+
+        assertTrue(tile.isRevealed(),"It should be true");
+
+    }
+
+
 
 }
