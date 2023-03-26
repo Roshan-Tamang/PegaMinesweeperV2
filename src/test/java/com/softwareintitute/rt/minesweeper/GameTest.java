0a0687790a758c610228b57f2eab699d47165b7e
Roshan Tamang <roshantamang@hotmail.co.uk>
2023-03-26 23:38:29 +0100

    Added unit tests for Game

--- src/test/java/com/softwareintitute/rt/minesweeper/GameTest.java
+++ src/test/java/com/softwareintitute/rt/minesweeper/GameTest.java
@@ -1,4 +1,39 @@
 package com.softwareintitute.rt.minesweeper;
 
-public class GameTest {
+import com.softwareintitute.rt.minesweeper.game.Game;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class GameTest {
+
+    @Test
+    void testing_game_getScore(){
+
+        Game game = new Game();
+
+        assertEquals(0,game.getScore(),"The values should match");
+
+    }
+
+    @Test
+    void testing_game_checkIfGameIsWon(){
+
+        Game game = new Game();
+
+        assertFalse(game.checkIfGameIsWon(),"It should be false");
+
+    }
+
+
+    @Test
+    void testing_game_checkIfGameIsOver(){
+
+        Game game = new Game();
+
+        assertFalse(game.checkIfGameIsOver(), "It should be false");
+
+    }
+
 }
